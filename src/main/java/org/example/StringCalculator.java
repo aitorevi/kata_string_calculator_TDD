eb570b058327c4478de8792aff269d7c50eb5969
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-04-04 13:21:30 +0200

    Test(green): handle_a_default_delimiter_resolves_sum_numbers

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -18,9 +18,13 @@ public class StringCalculator {
         }
         return 0;
     }
-
     private static List<Integer> transformTextToValues(String numbers) {
         // remplazar salto por coma
+        var regexForReplaceDelimiter = extractDelimiter(numbers);
+        if (!regexForReplaceDelimiter.equals("")){
+            var numbersTextOfSum = Arrays.stream(numbers.substring(6).split(regexForReplaceDelimiter)).toList();
+            return numbersTextOfSum.stream().map(Integer::valueOf).toList();
+        }
         var unifySeparator = numbers.replaceAll("\n",",");
         var numbersTextOfSum = Arrays.stream(unifySeparator.split(",")).toList();
         return numbersTextOfSum.stream().map(Integer::valueOf).toList();
@@ -28,6 +32,13 @@ public class StringCalculator {
 
     private static Integer sumAll(List<Integer> numbersIntOfSum) {
         return numbersIntOfSum.stream().reduce(0,Integer::sum);
+    }
+    private static String extractDelimiter(String numbers) {
+        var delimiter = "";
+        if (numbers.startsWith("//")) {
+            delimiter = numbers.substring(3,4);
+        }
+        return delimiter;
     }
 
 }
