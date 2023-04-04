13e1791a039865e62db616bd1a9be0c3f57ccd21
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-04-04 10:56:11 +0200

    Add .gitignore, create StringCalculator class, StringCalculatorTest and .gitignore

---
+++ src/main/java/org/example/StringCalculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class StringCalculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Integer Add(String numbers) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+}
