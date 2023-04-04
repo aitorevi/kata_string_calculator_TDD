2c4f250f6ef2e176863e0865c90a0fa7bba3f6b4
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-04-04 14:29:38 +0200

    Test(refactor): handle_a_default_delimiter_resolves_sum_numbers

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
